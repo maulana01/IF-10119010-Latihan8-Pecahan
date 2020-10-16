74a17783e9c6c1b3a703633d8af09ff3bbee1dab
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 13:50:19 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan8/pecahan/IF110119010Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA     : Maulana Imam Malik
+ * KELAS    : IF1
+ * NIM      : 10119010
+ * Deskripsi Program : Program ini berisi program untuk menampilkan value
+ *                     dengan tipe data pecahan
+ */
+public class IF110119010Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
